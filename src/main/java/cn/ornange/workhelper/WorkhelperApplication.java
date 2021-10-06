7dfd00177f028d36e63283a57721bb9043ebf380
nange1024@foxmail.com <nange1024@foxmail.com>
2021-10-06 15:36:25 +0800

    first commit

---
+++ src/main/java/cn/ornange/workhelper/WorkhelperApplication.java
@@ -0,0 +1,13 @@
+package cn.ornange.workhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkhelperApplication.class, args);
+    }
+
+}
